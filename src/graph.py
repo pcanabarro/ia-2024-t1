32d2820aba23fb0bfaf510f2d4639ec76da686b4
pcanabarro <pedroh.canabarro@gmail.com>
2024-05-01 17:18:31 -0300

    Implementing read_graph method using dictionary

--- src/graph.py
+++ src/graph.py
@@ -5,16 +5,38 @@ import sys
 
 def read_graph(filename: str):
     """Le uma estrutura de grafo de um arquivo e retorna a estrutura."""
-    graph = None
+    graph = {}
     with open(filename, "rt") as input_file:
-        vertex_count = int(input_file.readline().strip())
-        for _ in range(vertex_count):
+        vertex_count = int(input_file.readline().strip())  # File needs to start with coords quantity number
+
+        for i in range(vertex_count):  # Counts every coord
             index, latitude, longitude = input_file.readline().strip().split()
-            
-        edge_count = int(input_file.readline().strip())
-        for _ in range(edge_count):
+
+            graph[int(index)] = {'vertex': (float(latitude), float(longitude)), 'edges': []}
+
+        edge_count = int(input_file.readline().strip())  # File needs to have possibility quantity number after coords
+
+        for j in range(edge_count):  # Counts every possibility
             from_vertex, to_vertex, cost = (
                 input_file.readline().strip().split()
             )
-    
+
+            from_vertex, to_vertex, cost = int(from_vertex), int(to_vertex), float(cost)
+            graph[from_vertex]['edges'].append((to_vertex, cost))
+
     return graph
+
+# TODO: REMOVE THIS COMMENT - How mini map return dict should look like
+# graph = {
+#     0: {'coord': (-30.008646, -51.142909), 'edges': [(2, 93.16199226823055), (9, 370.84011298540076)]},
+#     1: {'coord': (-30.00894, -51.143344), 'edges': [(2, 40.19992641024175), (0, 53.13321306005169)]},
+#     2: {'coord': (-30.009126, -51.143702), 'edges': [(5, 128.56119211286216), (9, 278.0130930702961)]},
+#     3: {'coord': (-30.009509, -51.14423), 'edges': [(1, 106.2128991394714), (2, 66.32082803669168), (4, 44.93938270233482), (5, 62.612803936405655)]},
+#     4: {'coord': (-30.009731, -51.14462), 'edges': [(2, 17.77830304610445), (5, 81.63281218293855), (7, 137.6521663708679)]},
+#     5: {'coord': (-30.009801, -51.144786), 'edges': [(2, 128.56119211286216), (4, 17.77830304610445), (6, 81.63281218293855), (7, 119.89850426306808), (9, 150.92654043736871)]},
+#     6: {'coord': (-30.01014, -51.145538), 'edges': [(5, 81.63281218293855), (7, 38.37761880079564), (8, 57.258847924240776), (9, 69.64300121602454)]},
+#     7: {'coord': (-30.010327, -51.145873), 'edges': [(4, 137.6521663708679), (5, 119.89850426306808), (6, 38.37761880079564), (8, 20.481000375464983)]},
+#     8: {'coord': (-30.010342, -51.146085), 'edges': [(7, 20.481000375464983), (9, 12.941289400664326)]},
+#     9: {'coord': (-30.010351, -51.146219), 'edges': [(0, 370.84011298540076), (2, 278.0130930702961), (5, 150.92654043736871), (6, 69.64300121602454), (8, 12.941289400664326)]}
+# }
+
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 """Utilize este arquivo para depurar seus algoritmos."""
 
 from graph import read_graph
-from busca import a_star
+# from busca import a_star
 
 
 if __name__ == "__main__":
-    grafo = read_graph("vertices.txt")
-    vertices_avaliados, custo, caminho = a_star(grafo, 1, 100)
-    print(vertices_avaliados, custo, caminho)
+    mini_graph = read_graph("../mapas/mini_map.txt")
+    # micro_graph = read_graph("../mapas/micro_map.txt")
+    # small_graph = read_graph("../mapas/small_map.txt")
+    # medium_graph = read_graph("../mapas/medium_map.txt")
+    # full_graph = read_graph("../mapas/full_map.txt")
+    # vertices_avaliados, custo, caminho = a_star(grafo, 1, 100)
+    # print(vertices_avaliados, custo, caminho)
