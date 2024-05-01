f11a6d313e088ab0416af6629e139923e04a5802
Everton Santos de Castro <86254929+evertonsc@users.noreply.github.com>
2024-05-01 17:27:22 -0300

    Testando

--- src/busca/dfs.py
+++ src/busca/dfs.py
@@ -5,3 +5,4 @@ from util import reverse_path
 
 def dfs(graph, start: int, goal: int) -> (int, float, [int]):
     """Busca um caminho entre start e goal usando busca em profundidade."""
+    # teste
